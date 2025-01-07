28ba8235e948801ede3d17fe79844d9e0210ca8e
Akil <akilakil6874@gmail.com>
2025-01-07 14:41:06 +0530

    bug fixinggggg

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ const connectDB = async () => {
 dotenv.config();
 app.use(express.json());
 app.use("/images", express.static(imagesDir)); 
-app.use(cors({ origin: "https://blog-manage.vercel.app", credentials: true }));
+app.use(cors({ origin: "https://blog-manage.vercel.app", credentials: true, }));
 app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
